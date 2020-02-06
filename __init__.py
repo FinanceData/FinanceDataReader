c8e3469a371d1a764eb5c22683d864f4807e09fd
plusjune <plusjune@financedata.kr>
2020-02-06 12:34:01 +0900

    ADD dist 0.9.0

--- __init__.py
+++ __init__.py
@@ -1,7 +1,8 @@
 from .data import (DataReader)
 from .data import (StockListing)
+
 from .data import (EtfListing)
 
-__version__ = '0.8.5'
+__version__ = '0.9.0'
 
 __all__ = ['__version__', 'DataReader', 'StockListing', 'EtfListing']
