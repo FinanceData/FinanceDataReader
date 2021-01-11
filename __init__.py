e7facf34e3151b0cbefd20ec08229d42c6321a76
plusjune <plusjune@financedata.kr>
2021-01-11 13:01:31 +0900

    FIX: Index data US500, RUTNU FIX

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from .data import (DataReader)
 from .data import (StockListing)
 from .data import (EtfListing)
 
-__version__ = '0.9.11'
+__version__ = '0.9.13'
 
 __all__ = ['__version__', 'DataReader', 'StockListing', 'EtfListing']
