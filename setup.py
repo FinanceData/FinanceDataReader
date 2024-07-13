2ab2c6651ebf43c1681141195a8deeaa3e06dcb3
granzou <loopinf@gmail.com>
2024-07-13 12:59:29 +0900

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FinanceDataReader',
+    version='0.1.0',
+    author='loopinf',
+    author_email='loopinf@gmail.com',
+    description='A package for fetching and visualizing financial data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/loopinf/FinanceDataReader',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        # 'plotly',
+        # Add any other dependencies your package needs
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
